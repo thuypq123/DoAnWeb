f7d108b2826f2f000b71ca7bbd6d3a16fa1db2d4
Mkha85 <minhkha85201@gmail.com>
2022-06-08 17:24:07 +0700

    kha

--- WepApp/Controllers/ProductController.cs
+++ WepApp/Controllers/ProductController.cs
@@ -1,15 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using WepApp.Models;
 
 namespace WepApp.Controllers
 {
     public class ProductController : Controller
     {
+        public readonly covid19Context _context;
+        public ProductController()
+        {
+            _context = new covid19Context();
+        }
         public IActionResult Index()
         {
-            return View();
+            var viewSP = _context.Sanphams.ToList();
+            return View(viewSP);
         }
-        public IActionResult Details()
+        public IActionResult Details(int id)
         {
+            //var sanpham = _context.Sanphams.Include(x => x.Madm).FirstOrDefault(x => x.Masp == id);
+            //if (sanpham == null)
+            //{
+            //    return RedirectToAction("Index");
+            //}
             return View();
         }
         public IActionResult Pagecarts()
